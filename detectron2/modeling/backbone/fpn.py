e599ed4c78d3f9f4ad570504868b45c7f9e1e93d
emcotta1701c <141988532+emcotta1701c@users.noreply.github.com>
2024-03-28 14:11:42 -0700

    Update fpn.py
    build_convnextv2_fpn_backbone(), should take convnextv2_base() or so from cfg.MODEL.BACKBONE.NAME and build FPN on top of that.

--- detectron2/modeling/backbone/fpn.py
+++ detectron2/modeling/backbone/fpn.py
@@ -243,6 +243,30 @@ def build_resnet_fpn_backbone(cfg, input_shape: ShapeSpec):
     )
     return backbone
 
+# ConvNeXtV2 backbone with FPN; not in original detectron2 repo
+@BACKBONE_REGISTRY.register()
+def build_convnextv2_fpn_backbone(cfg, input_shape: ShapeSpec):
+    """
+    Args:
+        cfg: a detectron2 CfgNode
+
+    Returns:
+        backbone (Backbone): backbone module, must be a subclass of :class:`Backbone`.
+    """
+    # bottom_up = build_resnet_backbone(cfg, input_shape)
+    bottom_up = cfg.MODEL.BACKBONE.NAME()  # this needs to be tested
+    in_features = cfg.MODEL.FPN.IN_FEATURES
+    out_channels = cfg.MODEL.FPN.OUT_CHANNELS
+    backbone = FPN(
+        bottom_up=bottom_up,
+        in_features=in_features,
+        out_channels=out_channels,
+        norm=cfg.MODEL.FPN.NORM,
+        top_block=LastLevelMaxPool(),
+        fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
+    )
+    return backbone
+
 
 @BACKBONE_REGISTRY.register()
 def build_retinanet_resnet_fpn_backbone(cfg, input_shape: ShapeSpec):
